25972cfcbc231e265c26f3bcff2597a566a49ff7
Klenam Chris <ayokwashie42@gmail.com>
2026-03-27 01:13:12 +0000

    setting up foundation for hover effects

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,23 +1,36 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classnames from 'classnames';
 import React from "react";
 
 const NavBar = () => {
+
   const links = [
     { label: "Home", href: "/" },
     { label: "Services", href: "/services" },
     { label: "About", href: "/about" },
     { label: "FAQ", href: "/faq" },
   ];
+
+  const currentPath = usePathname();
+
   return (
-    <nav>
+    <nav className="flex space-x-6 px-5 mb-5 border-b h-14 items-center">
       <Link href={"/"}>Logo</Link>
-      <ul>
+      <ul className="flex space-x-6">
         {links.map((link) => (
           <li key={link.href}>
-            <Link href={link.href}>{link.label}</Link>
+            <Link href={link.href} className={classnames({
+                'text-zinc-900': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true,
+            })}>{link.label}</Link>
           </li>
         ))}
       </ul>
+      <button>Contact Us</button>
     </nav>
   );
 };
