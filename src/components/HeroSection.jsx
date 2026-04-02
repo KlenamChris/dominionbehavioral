617f595936711766288f0ff9594b372c6783219e
Klenam Chris <ayokwashie42@gmail.com>
2026-04-02 18:33:04 +0000

    minor bug fixes: reposition header tag on herosection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,24 +4,22 @@ import React from "react";
 const HeroSection = () => {
   return (
     <div
-      className="w-full h-screen bg-center bg-cover text-white"
+      className="flex justify-end items-center md:justify-center w-full h-screen bg-center bg-cover text-white"
       style={{
         backgroundImage: "url(home/3.jpg)",
       }}
       id="home"
     >
-      <div className="h-screen w-auto flex items-center justify-end md:mr-55 md:pr-28 md:pb-52 text-5xl md:text-6xl font-bold">
+      <div className="flex flex-col text-4xl md:text-6xl md:ml-74 font-bold">
         <div className="flex">
           <h2>Your Health</h2>
-          <hr className="border-t-2 w-18 my-8 mx-2" />
+          <hr className="border-t-2 w-18 my-6 md:my-8 mx-2" />
         </div>
-        <div className="absolute mt-32 mr-10">
-          <h2>Matters to Us</h2>
-        </div>
+        <h2>Matters to us</h2>
       </div>
       <div className="absolute text-white bottom-8 left-0 w-full flex justify-center">
         <button className="flex items-center animate-bounce px-3 py-3 bg-white/15  rounded-full duration-300 hover:bg-white/60 transition-all">
-          <ArrowDown className="w-4 h-4"/>
+          <ArrowDown className="w-4 h-4" />
         </button>
       </div>
     </div>
